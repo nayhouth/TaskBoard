86e8558c37381b59fec8c6972f0cc1910046721b
nayhouth <yem.nayhout@gmail.com>
2026-09-06 00:58:20 +0700

    Implement unit CRUD

---
+++ src/main/java/com/KBPRASAC/TaskBoard/unit/repository/UnitRepository.java
@@ -0,0 +1,6 @@
+package com.KBPRASAC.TaskBoard.unit.repository;
+
+import com.KBPRASAC.TaskBoard.unit.entity.Unit;
+import com.corebackend.repository.BaseRepository;
+
+public interface UnitRepository extends BaseRepository<Unit> {}
---
+++ src/main/java/com/KBPRASAC/TaskBoard/unit/service/UnitService.java
@@ -0,0 +1,6 @@
+package com.KBPRASAC.TaskBoard.unit.service;
+
+import com.KBPRASAC.TaskBoard.unit.entity.Unit;
+import com.corebackend.service.BaseService;
+
+public interface UnitService extends BaseService<Unit> {}
